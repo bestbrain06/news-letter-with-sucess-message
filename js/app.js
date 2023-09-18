5c8b35dbbc71fc42375d3a9043909fdcb3c4c376
bestbrain06 <136890174+bestbrain06@users.noreply.github.com>
2023-09-18 04:30:42 +0000

    added sucess.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,22 @@
 const form = document.querySelector("form");
 const input = document.querySelector("input");
-const error = document.querySelector("small");
+const small = document.querySelector("small");
 
 const emailRegex = /([A-Za-z0-9._-]+@[A-Za-z0-9._-]+\.[A-Za-z0-9]+)\w+/;
 
 form.addEventListener("submit", (e) => {
+  let error = false;
   let validEmail = emailRegex.test(input.value);
 
   if (!validEmail) {
-    error.textContent = " invalid  Email";
+    small.textContent = " Invalid  Email";
     input.style.borderColor = " hsl(4, 100%, 67%)";
+    error = true;
   } else {
     error.textContent = "";
+  }
+
+  if (error) {
+    e.preventDefault();
   }
 });
