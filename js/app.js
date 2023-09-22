e704b39ce6ef2ea997f12831dd8780c5a37d2c73
bestbrain06 <136890174+bestbrain06@users.noreply.github.com>
2023-09-22 13:33:36 +0000

    deleted the success html and sucess csss and sucess javascript

--- js/app.js
+++ js/app.js
@@ -1,22 +1,22 @@
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const small = document.querySelector("small");
+const form = document.querySelector('#form')
+const input = document.querySelector('input')
+const small = document.querySelector('small')
 
-const emailRegex = /([A-Za-z0-9._-]+@[A-Za-z0-9._-]+\.[A-Za-z0-9]+)\w+/;
+const emailRegex = /([A-Za-z0-9._-]+@[A-Za-z0-9._-]+\.[A-Za-z0-9]+)\w+/
 
-form.addEventListener("submit", (e) => {
-  let error = false;
-  let validEmail = emailRegex.test(input.value);
+form.addEventListener('submit', (e) => {
+  let error = false
+  let validEmail = emailRegex.test(input.value)
 
   if (!validEmail) {
-    small.textContent = " Invalid  Email";
-    input.style.borderColor = " hsl(4, 100%, 67%)";
-    error = true;
+    small.textContent = ' Invalid  Email'
+    input.style.borderColor = ' hsl(4, 100%, 67%)'
+    error = true
   } else {
-    error.textContent = "";
+    error.textContent = ''
   }
 
   if (error) {
-    e.preventDefault();
+    e.preventDefault()
   }
-});
+})
